6376d61b7f824e7396845a4619dd75cd7f38cfa3
unknown <pariharansh123@.com>
2022-11-03 14:59:16 +0530

    prime code fixed

---
+++ prime.js
@@ -0,0 +1,24 @@
+
+
+function checkPrime(num){
+ let count=0;
+ for(let i=1;i<=num;i++)
+   
+ {
+      if(num%i=0){
+        count++;
+       }
+   }
+if(count==2)
+{
+return true 
+    
+}
+return false;
+}
+let ans=checkPrime(123);
+if(ans==true)
+{
+console.log("Prime No");
+}else{
+console.log("Not Prime No")
